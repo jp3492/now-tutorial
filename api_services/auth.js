032fac358afd37b0ab6e88fb3ef48ad06d973acc
Jan-Philipp <jpm3492@gmail.com>
2019-10-02 13:48:01 +0200

    back to js

--- api_services/auth.js
+++ api_services/auth.js
@@ -15,8 +15,10 @@ export default async (req, res) => {
   const { accesstoken, cognitoid } = req.headers
   let verified
   try {
+    console.log("verifying");
     verified = await verifier.verify(accesstoken)
   } catch (error) {
+    console.error(error)
     res.status(400)
     return res.send({
       message: 'Couldnt verify accesstoken',
@@ -24,11 +26,13 @@ export default async (req, res) => {
     })
   }
   if (!verified) {
+    console.error("not authorized")
     res.status(401)
     res.send({ message: "You shall not pass - 'Gandalf'(long time ago)" })
   } else {
     try {
       const user = await User.findOne({ cognitoId: cognitoid })
+      console.log(user)
       return user._id
     } catch (error) {
       res.status(404)
